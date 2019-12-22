5edf168e2a6064195970b35e89b6a53682e32f1e
mozzerb2 <59149772+mozzerb2@users.noreply.github.com>
2019-12-22 21:53:48 +0000

    Add files via upload

---
+++ crypto.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("navbar-side").style.width = "100%";
+}
+function closeNav() {
+    document.getElementById("navbar-side").style.width = "0";
+}
+
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 60) {
+        $('.navbar').css('background','#02003F');
+    } else {
+        $('.navbar').css('background','transparent');
+    }
+});
